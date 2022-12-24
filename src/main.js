782929607dcecd6837533b08318ff12af0b68812
James-Livesey <jamesliveg@gmail.com>
2022-12-24 14:44:11 +0000

    Add start of crawler script

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/*
+    LiveG Open Search Index
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://search.liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+console.log("Hello, world!");
