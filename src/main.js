77a9d60273c302176ab13b7266540715cc618710
James-Livesey <jamesliveg@gmail.com>
2023-03-19 18:50:40 +0000

    Add saving of crawling state and better processing algorithm

--- src/main.js
+++ src/main.js
@@ -18,17 +18,20 @@ const fetch = function(...args) {
     });
 }
 
-const MAX_CRAWL_DEPTH = 10;
+const MAX_CRAWL_ADDITIONS = 5;
+const MAX_SELECT_TOP_TO_CRAWL = 100;
 const MIN_WORD_COUNT_AS_KEYWORD = 3;
-const CRAWL_TIMEOUT_DURATION = 5_000;
-const CRAWL_PAUSE_DURATION = 3_000;
+const CRAWL_TIMEOUT_DURATION = 5 * 1_000; // 5 seconds
+const CRAWL_PAUSE_DURATION = 3 * 1_000; // 3 seconds
+const PAGE_RECRAWL_WAIT_DURATION = 7 * 24 * 60 * 60 * 1_000; // 1 week
 
 const RE_MATCH_ALL_WORDS = /[\w']+/g;
 
 const INDEX_FIELDS = ["url", "title", "description", "firstIndexed", "lastUpdated", "referenceScore", "keywordScore"];
+const CRAWLED_LIST_FIELDS = ["url", "firstIndexed", "lastUpdated"];
 
 var pagesToCrawl = fs.readFileSync("data/tocrawl.txt", "utf-8").split("\n").filter((line) => line != "");
-var pagesCrawled = [];
+var pagesCrawled = {};
 var crawlStats = {added: 0, crawled: 0, skipped: 0};
 var indexes = {};
 
@@ -84,6 +87,24 @@ function objectsToTsv(data, fields) {
     return entryLines.join("\n");
 }
 
+function objectToArray(object, key) {
+    return Object.keys(object).map((keyValue) => ({...object[keyValue], [key]: keyValue}));
+}
+
+function arrayToObject(array, key) {
+    var object = {};
+
+    array.forEach(function(item) {
+        var keyValue = item[key];
+
+        delete item[key];
+
+        object[keyValue] = item;
+    });
+
+    return object;
+}
+
 function loadIndex(keyword) {
     var filePath = path.join("data", "indexes", `${keyword}.tsv`);
 
@@ -104,18 +125,30 @@ function saveIndex(keyword) {
     fs.writeFileSync(filePath, objectsToTsv(indexes[keyword], INDEX_FIELDS));
 }
 
+function saveCrawlLists() {
+    fs.writeFileSync("data/tocrawl.txt", pagesToCrawl.join("\n"));
+
+    fs.writeFileSync("data/crawled.tsv", objectsToTsv(objectToArray(pagesCrawled, "url"), CRAWLED_LIST_FIELDS));
+}
+
 function crawlPage(url) {
-    if (pagesCrawled.includes(url)) {
-        console.log(`Already crawled: ${url}`);
+    if (pagesCrawled.hasOwnProperty(url)) {
+        if (Date.now() - pagesCrawled[url].lastUpdated < PAGE_RECRAWL_WAIT_DURATION) {
+            console.log(`Already crawled within re-crawl wait duration: ${url}`);
 
-        crawlStats.skipped++;
+            crawlStats.skipped++;
 
-        return Promise.resolve();
+            return Promise.resolve();
+        }
     }
 
     console.log(`Crawling: ${url}`);
 
-    pagesCrawled.push(url); // TODO: Maybe save this to a file
+    pagesCrawled[url] ||= {};
+    pagesCrawled[url].firstIndexed ||= Date.now();
+    pagesCrawled[url].lastUpdated = Date.now();
+
+    saveCrawlLists();
 
     var controller = new AbortController();
     var signal = controller.signal;
@@ -138,8 +171,8 @@ function crawlPage(url) {
 
             dom.querySelectorAll("script, style, button, input, select, label").forEach((element) => element.remove());
 
-            var pageTitle = dom.querySelector("title")?.textContent;
-            var pageDescription = dom.querySelector("meta[name='description']")?.getAttribute("content");
+            var pageTitle = dom.querySelector("title")?.textContent.trim();
+            var pageDescription = (dom.querySelector("meta[name='description']")?.getAttribute("content") || "").trim();
             var pageText = dom.querySelector("body")?.textContent;
 
             if (!pageTitle || !pageText) {
@@ -157,13 +190,15 @@ function crawlPage(url) {
             wordSet.forEach(function(word) {
                 var entry = {
                     url,
-                    title: pageTitle,
-                    description: pageDescription || "",
+                    title: pageTitle.replace(/[\n\t]/g, ""),
+                    description: pageDescription.replace(/[\n\t]/g, ""),
                     firstIndexed: Date.now(),
                     lastUpdated: Date.now(),
                     referenceScore: 1 / 100,
                     keywordScore: Math.min(((commonWords[word] || 0) + (titleWords.includes(word) ? 5 : 0)) / 25, 1)
                 };
+
+                // TODO: Add language attribute
 
                 if (word.match(/\d{1,3}/)) {
                     return;
@@ -191,33 +226,32 @@ function crawlPage(url) {
                 saveIndex(word);
             });
 
-            [...dom.querySelectorAll("a[href]")].forEach(function(element) {
-                var reference = element.getAttribute("href");
-                var newUrl = new URL(reference, url).href.split("#")[0];
-                var shouldCrawl = true;
-
-                if (newUrl.startsWith("http://") || newUrl.startsWith("https://")) {
+            [...dom.querySelectorAll("a[href]")].forEach(function(element, i) {
+                if (i > MAX_CRAWL_ADDITIONS) {
                     return;
                 }
 
-                if (pagesToCrawl.includes(newUrl) || pagesCrawled.includes(newUrl)) {
-                    // TODO: Add freshness (page age) theshold for already-crawled pages
-                    shouldCrawl = false;
+                var reference = element.getAttribute("href");
+                var newUrl = new URL(reference, url).href.split("#")[0];
 
-                    if (new URL(url).host != new URL(newUrl).host) {
-                        // FIXME: Fix any potential cicular crawling issues
-                        shouldCrawl = true;
-                    }
+                if (!(newUrl.startsWith("http://") || newUrl.startsWith("https://"))) {
+                    return;
                 }
 
-                if (!shouldCrawl) {
+                if (pagesToCrawl.includes(newUrl)) {
                     return;
                 }
 
                 console.log(`Discovered page: ${newUrl}`);
 
-                pagesToCrawl.push(newUrl); // TODO: Maybe save this to a file
+                crawlStats.added++;
+
+                pagesToCrawl.push(newUrl); // TODO: This should ideally be added at a random location in the list
+
+                saveCrawlLists();
             });
+
+            fs.appendFileSync("data/corpus.txt", pageText + "\x04");
 
             console.log(`Crawl complete: ${url}`);
 
@@ -243,7 +277,14 @@ function getNextToCrawl() {
         return Promise.resolve();
     }
 
-    return crawlPage(pagesToCrawl.shift()).then(function() {
+    var index = Math.round((Math.random() ** 2) * Math.min(pagesToCrawl.length - 1, MAX_SELECT_TOP_TO_CRAWL)); // Algorithm to favour items near top of list
+    var pageToCrawl = pagesToCrawl[index];
+
+    pagesToCrawl.splice(index, 1);
+
+    saveCrawlLists();
+
+    return crawlPage(pageToCrawl).then(function() {
         return new Promise(function(resolve, reject) {
             console.log("Pausing...");
 
@@ -256,6 +297,12 @@ function getNextToCrawl() {
     }).then(function() {
         return getNextToCrawl();
     });
+}
+
+if (fs.existsSync("data/crawled.tsv")) {
+    pagesCrawled = arrayToObject(tsvToObjects(fs.readFileSync("data/crawled.tsv", "utf-8")), "url");
+
+    console.log("Loaded crawled pages list");
 }
 
 getNextToCrawl().then(function() {
